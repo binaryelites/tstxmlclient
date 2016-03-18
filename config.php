d86bc85572029557f6f65c7bb306d68d4ec44fb2
Arif <Arif@arif-cibl>
2016-03-19 01:48:48 +0600

    api changes

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("hostname", "http://localhost/tstnew/");
-//define("hostname", "http://travelshoptours.com/");
+//define("hostname", "http://localhost/tstnew/");
+define("hostname", "http://travelshoptours.com/");
 include_once("libraries/Requests.php");
 Requests::register_autoloader();
 
